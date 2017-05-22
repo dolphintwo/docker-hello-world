52c7c2ba5d5c046181d4f653239ae0e41847df11
zhongmengdie <zhong159880>
2017-05-22 10:20:51 +0800

    提交

---
+++ src/main/java/com/newtouch/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c)  2016, dabing.io
+ * All rights reserved.
+ * HelloWorld.java 2016-03-17 上午11:09
+ */
+
+package com.newtouch;
+
+/**
+ * 描述:TODO
+ *
+ * @author yangkui create on 2016-03-17.
+ * @since 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+
+}
